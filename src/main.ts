f85b2183c4da9c2556757e9493a5295a158f30c2
Pooja Mehra <poojamehra@Poojas-MacBook-Air.local>
2023-02-04 13:32:46 +0530

    added delay for hover tooltip and enhanced the parallax to work with touch too

--- src/main.ts
+++ src/main.ts
@@ -3,32 +3,58 @@ import './style.css'
 const track = document.querySelector<HTMLDivElement>('.work-preview');
 const images = document.querySelectorAll<HTMLImageElement>('.work-preview img');
 
-function changeLayout(percentage: number){
-  const nextPercentageUnconstrained = parseFloat(track!.dataset.prevPercentage as string) + percentage ;
 
-  const nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 10), -10);
+const handleDownEvent = (e:number) => {
+    track!.dataset.mouseAt = `${e}`;
+};
 
-  track!.dataset.percentage = `${nextPercentage}` ;
+const handleUpEvent = () => {
+
+  track!.dataset.mouseAt = "0";
+  track!.dataset.prevPercentage = `${track!.dataset.percentage}`;
+}
+
+function changeLayout(deltaPercentage:number){
+
+  const prevPercentage = track!.dataset.prevPercentage as string;
+  const unconstrainedPercentage = deltaPercentage + parseInt(prevPercentage);
+  const nextPercentage = Math.max(Math.min(unconstrainedPercentage, 2), -20);
+  track!.dataset.percentage = `${nextPercentage}`;
 
   track?.animate({
-    transform: `translate(${nextPercentage}%, 0)`
-  }, {duration: 1200, fill:'forwards'});
+        transform: `translate(${nextPercentage}%, 0)`, animationTimingFunction:`ease-in`
+      }, {duration: 1200, fill:'forwards'});
+    
+      images.forEach((img,idx) => {
+        img.animate({
+          objectPosition: `${50+nextPercentage}% center`
+        }, {duration: 1200, fill:'forwards'});
+      })
+}
 
-  images.forEach((img,idx) => {
-    img.animate({
-      objectPosition: `${50+nextPercentage}% center`
-    }, {duration: 1200, fill: 'forwards'});
-  })
+const handleTrackMove = (e:number) => {
+  if(track!.dataset.mouseAt === '0' ) return;
+
+  const initialTrack = track!.dataset.mouseAt as any; 
+  const delta = (e - initialTrack)/(window.innerWidth);
+
+  const deltaPercentage = delta*50;
+
+   changeLayout(deltaPercentage);
 
 
 }
 
-track!.onmousemove = e => {
-  const mouseX = e.clientX;
-  const xDecimal = mouseX/window.innerWidth;
-  const maxX = track!.offsetWidth - window.innerWidth;
 
-  const panX = maxX * xDecimal * -1;
+track!.onmousemove = e =>  handleTrackMove(-e.clientX);
 
-  changeLayout(panX/10);
-}
+track!.onmouseenter = e => handleDownEvent(-e.clientX);
+
+track!.onmouseleave = _ => handleUpEvent();
+
+track!.ontouchstart = e => handleDownEvent(e.touches[0].clientX);
+
+track!.ontouchmove = (e) => handleTrackMove(e.touches[0].clientX);
+
+track!.ontouchend =(e) => handleTrackMove(e.touches[0].clientX);
+
