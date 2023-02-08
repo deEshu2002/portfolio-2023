ae44a80c17f95c61ed4cda589e70a4f26713bafe
Pooja Mehra <poojamehra@Poojas-MacBook-Air.local>
2023-02-08 22:07:10 +0530

    changed the flow of document for better scalability

--- src/main.ts
+++ src/main.ts
@@ -19,9 +19,8 @@ window.addEventListener('mousewheel', (e) => {
   //   scrolled to see top
   // }
   
-  // card?.animate({transform:`scale(1,3.5)`,postion:`absolute`, transformOrigin: `bottom`}, {duration:1200, fill: 'forwards'})
 
-  // card?.animate({width:`100vw`}, {duration:1200, fill:'forwards'})
+  // card?.animate({minWidth:`100vw`}, {duration:1200, fill:'forwards'})
 
 }, false) 
 
