bd4f140719f261bcaff8a64a14cb9a7ba0f25694
Pooja Mehra <poojamehra@Poojas-MacBook-Air.local>
2023-02-18 18:55:36 +0530

    made svg visible

--- deploy.sh
+++ deploy.sh
@@ -21,9 +21,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:deEshu2002/deEshu2002.github.io.git main
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:deEshu2002/portfolio-2023.git main:gh-pages
 
 cd -
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,3 @@
 export default{
-    base:'/',
+    base:'/portfolio-2023/',
 }
