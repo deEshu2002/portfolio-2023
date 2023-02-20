962c77a677d8da3a9ff81fba8cb1a2b19c53ebb8
Deepanshu <deepanshumehra2001@gmail.com>
2023-02-20 08:06:35 +0530

    changed the image links to get a good UX od parallax

--- src/OffLoadValues.ts
+++ src/OffLoadValues.ts
@@ -1,76 +1,76 @@
 
 const Content = {
     "one":{
-        backgroundImage:'https://source.unsplash.com/HM-Y497t5CU',
+        backgroundImage:'https://source.unsplash.com/omKHuMLlDI0',
         titleHeading:'First-Commerce',
         titleSubHeading:'2002',
-        overViewImage:'', 
+        overViewImage:'https://source.unsplash.com/omKHuMLlDI0', 
         descriptionPara:["Lorem ipsum dolor sit amet consectetur adipiscing","elit Ut Aliquam in hendrerit urna. Pellentesque sit","amet sapien fringilla, mattisa","Done with my Self Cause you are my angel","don't wait cause you ", ],
         tools:["React","Figma","TailwindCss","React Router","ReactQuery"],
-        productImage:["https://source.unsplash.com/HM-Y497t5CU","https://source.unsplash.com/HM-Y497t5CU"]
+        productImage:["https://source.unsplash.com/omKHuMLlDI0","https://source.unsplash.com/omKHuMLlDI0"]
     },
     "two":{
-        backgroundImage:'/iPhone2.jpg',
+        backgroundImage:'https://source.unsplash.com/8b1cWDyvT7Y',
         titleHeading:'Secont-Commerce',
         titleSubHeading:'2003',
-        overViewImage:'/iPhone2.jpg', 
+        overViewImage:'https://source.unsplash.com/8b1cWDyvT7Y', 
         descriptionPara:["Lorem ipsum dolor sit amet consectetur adipiscing","elit Ut Aliquam in hendrerit urna. Pellentesque sit","amet sapien fringilla, mattisa","Done with my Self Cause you are my angel","don't wait cause you ", ],
         tools:["React","Figma","TailwindCss","React Router","ReactQuery"],
-        productImage:["/iPhone2.jpg","/iPhone2.jpg"]
+        productImage:["https://source.unsplash.com/8b1cWDyvT7Y","https://source.unsplash.com/8b1cWDyvT7Y"]
     },
     "three":{
-        backgroundImage:'http://t2.gstatic.com/images?q=tbn:ANd9GcT887BhlZxtVFFnFpdm3UfuhRiPfArrqBc057f33k3lnIYB9xiE',
+        backgroundImage:'https://source.unsplash.com/pRt0ktgl31s',
         titleHeading:'Third-Commerce',
         titleSubHeading:'2004',
-        overViewImage:'http://t2.gstatic.com/images?q=tbn:ANd9GcT887BhlZxtVFFnFpdm3UfuhRiPfArrqBc057f33k3lnIYB9xiE', 
+        overViewImage:'https://source.unsplash.com/pRt0ktgl31s', 
         descriptionPara:["Lorem ipsum dolor sit amet consectetur adipiscing","elit Ut Aliquam in hendrerit urna. Pellentesque sit","amet sapien fringilla, mattisa","Done with my Self Cause you are my angel","don't wait cause you ", ],
         tools:["React","Figma","TailwindCss","React Router","ReactQuery"],
-        productImage:["http://t2.gstatic.com/images?q=tbn:ANd9GcT887BhlZxtVFFnFpdm3UfuhRiPfArrqBc057f33k3lnIYB9xiE","http://t2.gstatic.com/images?q=tbn:ANd9GcT887BhlZxtVFFnFpdm3UfuhRiPfArrqBc057f33k3lnIYB9xiE"]
+        productImage:["https://source.unsplash.com/pRt0ktgl31s","https://source.unsplash.com/pRt0ktgl31s"]
     },
     "four":{
-        backgroundImage:'/iphone-mockup.png',
+        backgroundImage:'https://source.unsplash.com/FdNi_4Jg3_E',
         titleHeading:'Fourth-Commerce',
         titleSubHeading:'2005',
-        overViewImage:'/iphone-mockup.png', 
+        overViewImage:'https://source.unsplash.com/FdNi_4Jg3_E', 
         descriptionPara:["Lorem ipsum dolor sit amet consectetur adipiscing","elit Ut Aliquam in hendrerit urna. Pellentesque sit","amet sapien fringilla, mattisa","Done with my Self Cause you are my angel","don't wait cause you ", ],
         tools:["React","Figma","TailwindCss","React Router","ReactQuery"],
-        productImage:["/iphone-mockup.png","/iphone-mockup.png"]
+        productImage:["https://source.unsplash.com/FdNi_4Jg3_E","https://source.unsplash.com/FdNi_4Jg3_E"]
     },
     "five":{
-        backgroundImage:'https://source.unsplash.com/8TSqJoI-NVs',
+        backgroundImage:'https://source.unsplash.com/hpjSkU2UYSU',
         titleHeading:'Fifth-Commerce',
         titleSubHeading:'2006',
-        overViewImage:'https://source.unsplash.com/8TSqJoI-NVs', 
+        overViewImage:'https://source.unsplash.com/hpjSkU2UYSU', 
         descriptionPara:["Lorem ipsum dolor sit amet consectetur adipiscing","elit Ut Aliquam in hendrerit urna. Pellentesque sit","amet sapien fringilla, mattisa","Done with my Self Cause you are my angel","don't wait cause you ", ],
         tools:["React","Figma","TailwindCss","React Router","ReactQuery"],
-        productImage:["https://source.unsplash.com/8TSqJoI-NVs","https://source.unsplash.com/8TSqJoI-NVs"]
+        productImage:["https://source.unsplash.com/hpjSkU2UYSU","https://source.unsplash.com/hpjSkU2UYSU"]
     },
     "six":{
-        backgroundImage:'https://source.unsplash.com/vazjaqOpY9g',
+        backgroundImage:'https://source.unsplash.com/Im7lZjxeLhg',
         titleHeading:'Sixth-Commerce',
         titleSubHeading:'2007',
-        overViewImage:'https://source.unsplash.com/vazjaqOpY9g', 
+        overViewImage:'https://source.unsplash.com/Im7lZjxeLhg', 
         descriptionPara:["Lorem ipsum dolor sit amet consectetur adipiscing","elit Ut Aliquam in hendrerit urna. Pellentesque sit","amet sapien fringilla, mattisa","Done with my Self Cause you are my angel","don't wait cause you ", ],
         tools:["React","Figma","TailwindCss","React Router","ReactQuery"],
-        productImage:["https://source.unsplash.com/vazjaqOpY9g","https://source.unsplash.com/vazjaqOpY9g"]
+        productImage:["https://source.unsplash.com/Im7lZjxeLhg","https://source.unsplash.com/Im7lZjxeLhg"]
     },
     "seven":{
-        backgroundImage:'https://source.unsplash.com/HM-Y497t5CU',
+        backgroundImage:'https://source.unsplash.com/p44B3PKChs4',
         titleHeading:'Seventh-Commerce',
         titleSubHeading:'2008',
-        overViewImage:'https://source.unsplash.com/HM-Y497t5CU', 
+        overViewImage:'https://source.unsplash.com/p44B3PKChs4', 
         descriptionPara:["Lorem ipsum dolor sit amet consectetur adipiscing","elit Ut Aliquam in hendrerit urna. Pellentesque sit","amet sapien fringilla, mattisa","Done with my Self Cause you are my angel","don't wait cause you ", ],
         tools:["React","Figma","TailwindCss","React Router","ReactQuery"],
-        productImage:["https://source.unsplash.com/HM-Y497t5CU","https://source.unsplash.com/HM-Y497t5CU"]
+        productImage:["https://source.unsplash.com/p44B3PKChs4","https://source.unsplash.com/p44B3PKChs4"]
     },
     "eight":{
-        backgroundImage:'https://source.unsplash.com/-R3Mi_Y1JQ4',
+        backgroundImage:'https://source.unsplash.com/hUD0PUczwJQ',
         titleHeading:'Eight-Commerce',
         titleSubHeading:'2009',
-        overViewImage:'https://source.unsplash.com/-R3Mi_Y1JQ4', 
+        overViewImage:'https://source.unsplash.com/hUD0PUczwJQ', 
         descriptionPara:["Lorem ipsum dolor sit amet consectetur adipiscing","elit Ut Aliquam in hendrerit urna. Pellentesque sit","amet sapien fringilla, mattisa","Done with my Self Cause you are my angel","don't wait cause you ", ],
         tools:["React","Figma","TailwindCss","React Router","ReactQuery"],
-        productImage:["https://source.unsplash.com/-R3Mi_Y1JQ4","https://source.unsplash.com/-R3Mi_Y1JQ4"]
+        productImage:["https://source.unsplash.com/hUD0PUczwJQ","https://source.unsplash.com/hUD0PUczwJQ"]
     }
 }
 
